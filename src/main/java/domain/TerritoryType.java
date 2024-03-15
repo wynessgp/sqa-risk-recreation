5905d6d609d9e5fdd59790a2c68da2514b304145
wynessgp <wynessgp@rose-hulman.edu>
2024-03-15 16:54:20 -0400

    Pushing the remaining missing territories

--- src/main/java/domain/TerritoryType.java
+++ src/main/java/domain/TerritoryType.java
@@ -1,10 +1,60 @@
 package domain;
 
 public enum TerritoryType {
-    AFGHANISTAN, 
+
+    // Copying territories as found on the Wikipedia page:
+    // https://en.wikipedia.org/wiki/Risk_(game)
+
+    // North America
     ALASKA, 
-    WESTERN_EUROPE, 
-    WESTERN_AUSTRALIA, 
+    ALBERTA,
+    CENTRAL_AMERICA,
+    EASTERN_UNITED_STATES,
+    GREENLAND,
+    ONTARIO,
+    QUEBEC,
+    WESTERN_UNITED_STATES,
+    
+    // South America
     ARGENTINA, 
-    EGYPT
+    BRAZIL,
+    PERU,
+    VENEZUELA,
+
+    // Europe
+    EASTERN_EUROPE,
+    GREAT_BRITAIN,
+    ICELAND,
+    NORTHERN_EUROPE,
+    SCANDINAVIA,
+    SOUTHERN_EUROPE,
+    WESTERN_EUROPE,
+
+    // Africa
+    CONGO,
+    EAST_AFRICA,
+    EGYPT,
+    MADAGASCAR,
+    NORTH_AFRICA,
+    SOUTH_AFRICA,
+
+    // Asia
+    AFGHANISTAN,
+    CHINA,
+    INDIA,
+    IRKUTSK,
+    JAPAN,
+    KAMCHATKA,
+    MIDDLE_EAST,
+    MONGOLIA,
+    SIAM,
+    SIBERIA,
+    URAL,
+    YAKUTSK,
+    
+    // Oceania
+    EASTERN_AUSTRALIA,
+    INDONESIA,
+    NEW_GUINEA,
+    WESTERN_AUSTRALIA 
 }
