4edeeeba9317258d84ace2962946892d0ab98565
wynessgp <wynessgp@rose-hulman.edu>
2024-03-21 10:01:27 -0400

    Small refactor to test cases to no longer include a magic number

--- src/test/java/domain/DieTest.java
+++ src/test/java/domain/DieTest.java
@@ -42,7 +42,7 @@ public class DieTest {
         // but we'll expect it to return 6 (since the die is technically capable of it)
         // Note that we also need to return forcedRoll - 1, as we add the lower bound due to JDK11's restrictions.
         EasyMock.expect(random.nextInt((maximumPossibleRoll - minimumPossibleRoll) + 1))
-                .andReturn(forcedRollForMock - 1);
+                .andReturn(forcedRollForMock - minimumPossibleRoll);
 
         // Replay
         EasyMock.replay(random);
@@ -56,5 +56,31 @@ public class DieTest {
 
         // Assert like normal
         assertEquals(forcedRollForMock, actualResult);
+    }
+
+    @Test
+    public void test02_validRandomPointer_higherMaximalRoll_expect14() {
+        int maximumPossibleRoll = 27;
+        int minimumPossibleRoll = 1;
+        int expected = 14;
+        // Record
+        Random random = EasyMock.mock(Random.class);
+
+        // Note that we also need to return forcedRoll - 1, as we add the lower bound due to JDK11's restrictions.
+        EasyMock.expect(random.nextInt((maximumPossibleRoll - minimumPossibleRoll) + 1))
+                .andReturn(expected - minimumPossibleRoll);
+
+        // Replay
+        EasyMock.replay(random);
+
+        // Regular JUnit testing code
+        Die unitUnderTest = new Die(maximumPossibleRoll, minimumPossibleRoll);
+        Integer actualResult = unitUnderTest.rollSingleDie(random);
+
+        // Verify
+        EasyMock.verify(random);
+
+        // Assert like normal
+        assertEquals(expected, actualResult);
     }
 }
