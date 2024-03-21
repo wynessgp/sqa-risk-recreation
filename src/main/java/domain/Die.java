4576aac28ad5debd5f7136cd658be266be41c41e
wynessgp <wynessgp@rose-hulman.edu>
2024-03-21 09:13:24 -0400

    Need to swap IDEs, pushing what I have

--- src/main/java/domain/Die.java
+++ src/main/java/domain/Die.java
@@ -4,9 +4,23 @@ import java.util.Random;
 
 public class Die {
 
-    public void rollSingleDie(Random randomizer) {
+    private int maximumPossibleRoll;
+    private int minimumPossibleRoll;
+
+    protected Die() {
+        this(0, 0); // for testing purposes.
+    }
+
+    public Die(int minimumPossibleRoll, int maximumPossibleRoll) {
+        this.maximumPossibleRoll = maximumPossibleRoll;
+        this.minimumPossibleRoll = minimumPossibleRoll;
+    }
+
+    public Integer rollSingleDie(Random randomizer) {
         if (randomizer == null) 
             throw new NullPointerException("Randomizer object is null, cannot roll Die!");
+        // JDK 11 doesn't include the lowerbounded nextInt method; so we offset the maximum
+        return randomizer.nextInt(maximumPossibleRoll - minimumPossibleRoll) + minimumPossibleRoll;
     }
 
 }
--- src/test/java/domain/DieTest.java
+++ src/test/java/domain/DieTest.java
@@ -3,7 +3,13 @@ package domain;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Random;
+
+import org.easymock.EasyMock;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class DieTest {
     
@@ -20,5 +26,33 @@ public class DieTest {
         // assert
         String actualMessage = exception.getMessage();
         assertEquals(expectedMessage, actualMessage);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 6})
+    public void test01_validRandomPointer_expectBVASuggestedValues(int forcedRollForMock) {
+        // variable setup: note that JDK11 doesn't have the lowerbounded nextInt,
+        // so we have to offset the max and then re-add in the minimum later.
+        int maximumPossibleRoll = 6;
+        int minimumPossibleRoll = 1;
+
+        // Record
+        Random random = EasyMock.mock(Random.class);
+        // this line is a bit weird for the 6 case, as we'll be limited to 5 as the max in rand.nextInt, 
+        // but we'll expect it to return 6 (since the die is technically capable of it)
+        EasyMock.expect(random.nextInt(maximumPossibleRoll - minimumPossibleRoll)).andReturn(forcedRollForMock);
+
+        // Replay
+        EasyMock.replay(random);
+
+        // Regular JUnit testing code
+        Die unitUnderTest = new Die(maximumPossibleRoll, minimumPossibleRoll);
+        Integer actualResult = unitUnderTest.rollSingleDie(random);
+
+        // Verify
+        EasyMock.verify(random);
+
+        // Assert like normal
+        assertEquals(forcedRollForMock, actualResult);
     }
 }
