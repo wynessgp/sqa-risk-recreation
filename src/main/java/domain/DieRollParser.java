b52a411d2ac1a1eb4ea680526e317b007e8f1396
wynessgp <wynessgp@rose-hulman.edu>
2024-03-22 09:41:46 -0400

    test01RollAttackerDiceTooFewOrTooManyDice by TDD standards

--- src/main/java/domain/DieRollParser.java
+++ src/main/java/domain/DieRollParser.java
@@ -5,4 +5,10 @@ public class DieRollParser {
     public boolean buildDiceLists() {
         return true;
     }
+
+    public void rollAttackerDice(int amountOfDiceToRoll) {
+        if (amountOfDiceToRoll < 1 || amountOfDiceToRoll > 3) {
+            throw new IllegalArgumentException("Valid amount of dice is in the range [1, 3]");
+        }
+    }
 }
--- src/test/java/domain/DieRollParserTest.java
+++ src/test/java/domain/DieRollParserTest.java
@@ -1,8 +1,12 @@
 package domain;
 
-import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class DieRollParserTest {
 
@@ -13,5 +17,24 @@ public class DieRollParserTest {
 
         // perform the operation
         assertTrue(unitUnderTest.buildDiceLists());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 4})
+    public void test01_rollAttackerDice_tooFewOrTooManyDice_expectException(int illegalInput) {
+        // variable setup
+        DieRollParser unitUnderTest = new DieRollParser();
+
+        // preliminary operation:
+        assertTrue(unitUnderTest.buildDiceLists());
+
+        // perform the operation
+        String expectedMessage = "Valid amount of dice is in the range [1, 3]";
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> unitUnderTest.rollAttackerDice(illegalInput));
+
+        // assert
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
     }
 }
