5c9d0fb196931ee0cf3d601138d8477e4b6f5258
wynessgp <wynessgp@rose-hulman.edu>
2024-03-23 15:35:51 -0400

    test03RollAttackerDiceRollThreeDiceExpectSortedListOfSizeThree passes by TDD standards

--- src/main/java/domain/DieRollParser.java
+++ src/main/java/domain/DieRollParser.java
@@ -1,14 +1,23 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 public class DieRollParser {
 
-    private List<Die> attackerDice;
+    private final List<Die> attackerDice;
+    private Random randomizer;
 
     public DieRollParser() {
         this.attackerDice = new ArrayList<>();
+    }
+
+    // this method is to only be utilized for unit testing!
+    DieRollParser(Random randomizer, List<Die> attackerDice) {
+        this.randomizer = randomizer;
+        this.attackerDice = attackerDice;
     }
 
     public boolean buildDiceLists() {
@@ -22,7 +31,7 @@ public class DieRollParser {
         return true;
     }
 
-    public void rollAttackerDice(int amountOfDiceToRoll) {
+    public List<Integer> rollAttackerDice(int amountOfDiceToRoll) {
         if (amountOfDiceToRoll < 1 || amountOfDiceToRoll > 3) {
             throw new IllegalArgumentException("Valid amount of dice is in the range [1, 3]");
         }
@@ -31,5 +40,14 @@ public class DieRollParser {
             throw new IllegalArgumentException(
                     "Not enough dice to fulfill requested amount to roll");
         }
+
+        List<Integer> rollResults = new ArrayList<>();
+        if (amountOfDiceToRoll == 3) {
+            rollResults.add(attackerDice.get(0).rollSingleDie(randomizer));
+            rollResults.add(attackerDice.get(1).rollSingleDie(randomizer));
+            rollResults.add(attackerDice.get(2).rollSingleDie(randomizer));
+        }
+        rollResults.sort(Comparator.reverseOrder());
+        return rollResults;
     }
 }
--- src/test/java/domain/DieRollParserTest.java
+++ src/test/java/domain/DieRollParserTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+import java.util.Random;
+import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -57,5 +60,47 @@ public class DieRollParserTest {
         // assert
         String actualMessage = exception.getMessage();
         assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void test03_rollAttackerDice_rollThreeDice_expectSortedListOfSizeThree() {
+        // variable setup
+        Random random = new Random();
+
+        Die firstDie = EasyMock.mock(Die.class);
+        Die secondDie = EasyMock.mock(Die.class);
+        Die thirdDie = EasyMock.mock(Die.class);
+
+        // record - note these are in UNSORTED order!
+        EasyMock.expect(firstDie.rollSingleDie(random)).andReturn(1);
+        EasyMock.expect(secondDie.rollSingleDie(random)).andReturn(3);
+        EasyMock.expect(thirdDie.rollSingleDie(random)).andReturn(2);
+
+        // provide the underlying dice collection to our class.
+        DieRollParser unitUnderTest = new DieRollParser(random,
+                List.of(firstDie, secondDie, thirdDie));
+
+        // replay
+        EasyMock.replay(firstDie, secondDie, thirdDie);
+
+        // do regular JUnit test calculations
+        List<Integer> actual = unitUnderTest.rollAttackerDice(3);
+
+        // verify
+        EasyMock.verify(firstDie, secondDie, thirdDie);
+
+        // do regular JUnit assertions
+        assertEquals(3, actual.size());
+        assertTrue(isSortedInNonIncreasingOrder(actual));
+        assertEquals(List.of(3, 2, 1), actual);
+    }
+
+    private boolean isSortedInNonIncreasingOrder(List<Integer> listInQuestion) {
+        for (int i = 0; i < listInQuestion.size() - 1; i++) {
+            if (listInQuestion.get(i) < listInQuestion.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
