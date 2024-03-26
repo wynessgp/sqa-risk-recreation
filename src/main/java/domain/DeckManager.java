c9ecc62d7aa926bf16c6929cd39ae627675669b9
RHIT-Harvey Yang <45359374+GMyoung@users.noreply.github.com>
2024-03-26 11:25:25 -0400

    first test for initDeck

--- src/main/java/domain/DeckManager.java
+++ src/main/java/domain/DeckManager.java
@@ -1,4 +1,36 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class DeckManager {
+    private List<Card> deckOfCards;
+
+    public DeckManager() {
+        this.deckOfCards = new ArrayList<>();
+    }
+
+    public boolean initDeck() {
+
+        if (!this.deckOfCards.isEmpty()) {
+            return false;
+        }
+
+        for (TerritoryType territory : TerritoryType.values()) {
+            this.deckOfCards.add(new TerritoryCard(territory, PieceType.INFANTRY));
+        }
+
+        this.deckOfCards.add(new WildCard());
+        this.deckOfCards.add(new WildCard());
+
+
+        return true;
+    }
+
+    public List<Card> getDeckOfCards() {
+        return this.deckOfCards;
+    }
+
+    
 }
--- src/test/java/domain/DeckManagerTest.java
+++ src/test/java/domain/DeckManagerTest.java
@@ -7,12 +7,34 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.easymock.EasyMock;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 public class DeckManagerTest {
+
+    private DeckManager deckManager;
+    @BeforeEach
+    public void setup() {
+        deckManager = new DeckManager();
+    }
+
+    @Test
+    public void test01_initDeck_EmptyCollection_ExpectSuccess() {
+        boolean result = deckManager.initDeck();
+
+        assertTrue(result);
+
+        List<Card> deck = deckManager.getDeckOfCards();
+        assertEquals(44, deck.size());
+        //no checking for card type yet
+
+    }
+
 
 
 }
