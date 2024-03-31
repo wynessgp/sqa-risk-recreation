cc7c6df336c353d573c604547b262388a0719ad9
Canon <22649817+cm090@users.noreply.github.com>
2024-03-31 13:08:35 -0400

    test00_drawCard_withEmptyDeck_throwsException passes

--- src/main/java/domain/DeckManager.java
+++ src/main/java/domain/DeckManager.java
@@ -1,48 +1,10 @@
 package domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import java.util.NoSuchElementException;
 
 public class DeckManager {
-    private final List<Card> deckOfCards;
 
-    public DeckManager() {
-        this.deckOfCards = new ArrayList<>();
+    public Card drawCard() {
+        throw new NoSuchElementException("Cannot draw card from an empty deck");
     }
-
-    protected DeckManager(List<Card> deckOfCards) {
-        this.deckOfCards = deckOfCards;
-    }
-
-    public boolean initDeck() {
-        if (!this.deckOfCards.isEmpty()) {
-            throw new IllegalStateException("Cannot initialize a non-empty deck.");
-        }
-        generateTerritoryCards();
-        this.deckOfCards.add(new WildCard());
-        this.deckOfCards.add(new WildCard());
-        return true;
-    }
-
-    private void generateTerritoryCards() {
-        for (TerritoryType territory : TerritoryType.values()) {
-            this.deckOfCards.add(new TerritoryCard(territory, PieceType.INFANTRY));
-        }
-    }
-
-    public boolean shuffle() {
-        if (this.deckOfCards.isEmpty()) {
-            return false;
-        }
-
-        Collections.shuffle(this.deckOfCards);
-        return true;
-    }
-
-    protected int getDeckSize() {
-        return this.deckOfCards.size();
-    }
-
 }
