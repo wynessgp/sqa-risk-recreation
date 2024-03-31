49cca68ef4bacabe00dbb704b298e735475b10c9
Canon <22649817+cm090@users.noreply.github.com>
2024-03-31 13:47:52 -0400

    test01_drawCard_withCompleteDeck_drawUntilEmpty passes

--- src/main/java/domain/DeckManager.java
+++ src/main/java/domain/DeckManager.java
@@ -1,10 +1,28 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class DeckManager {
+    List<Card> deckOfCards;
+
+    public DeckManager() {
+        this.deckOfCards = new ArrayList<>();
+    }
 
     public Card drawCard() {
-        throw new NoSuchElementException("Cannot draw card from an empty deck");
+        if (isDeckEmpty()) {
+            throw new NoSuchElementException("Cannot draw card from an empty deck");
+        }
+        return deckOfCards.remove(deckOfCards.size() - 1);
+    }
+
+    void setDeck(List<Card> deck) {
+        this.deckOfCards = new ArrayList<>(deck);
+    }
+
+    boolean isDeckEmpty() {
+        return deckOfCards.isEmpty();
     }
 }
--- src/test/java/domain/DeckManagerTest.java
+++ src/test/java/domain/DeckManagerTest.java
@@ -1,9 +1,15 @@
 package domain;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.NoSuchElementException;
+import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 
 public class DeckManagerTest {
@@ -15,5 +21,26 @@ public class DeckManagerTest {
 
         Exception exception = assertThrows(NoSuchElementException.class, deckManager::drawCard);
         assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    public void test01_drawCard_withCompleteDeck_drawUntilEmpty() {
+        DeckManager deckManager = new DeckManager();
+        int startingSize = 44;
+        List<Card> mockedCards = new ArrayList<>();
+
+        for (int i = 0; i < startingSize; i++) {
+            Card cardToAdd = EasyMock.createMock(Card.class);
+            EasyMock.replay(cardToAdd);
+            mockedCards.add(cardToAdd);
+        }
+        deckManager.setDeck(mockedCards);
+
+        for (int i = startingSize - 1; i >= 0; i--) {
+            Card drawnCard = assertDoesNotThrow(deckManager::drawCard);
+            assertEquals(mockedCards.get(i), drawnCard);
+            EasyMock.verify(drawnCard);
+        }
+        assertTrue(deckManager.isDeckEmpty());
     }
 }
