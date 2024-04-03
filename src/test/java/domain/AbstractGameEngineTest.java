8571b2144f1bc12d1761fecf65ce2973bca56b27
wynessgp <wynessgp@rose-hulman.edu>
2024-04-02 23:26:12 -0400

    Refactored test 00 to be test00InitializePlayersListAllIllegalInputsExpectException, now parameterized

--- src/test/java/domain/AbstractGameEngineTest.java
+++ src/test/java/domain/AbstractGameEngineTest.java
@@ -4,22 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class AbstractGameEngineTest {
 
     // this testing file will only test things ALL the win conditions share.
     // namely, the non-abstract methods in GameEngine.
 
-    @Test
-    public void test00_initializePlayersList_onePlayerInput_expectException() {
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 1, 7})
+    public void test00_initializePlayersList_allIllegalInputs_expectException(int illegalInput) {
         GameEngine unitUnderTest = new WorldDominationGameEngine();
         List<PlayerColor> emptyPlayerColorList = List.of();
-        int amountOfPlayers = 1;
 
         String expectedMessage = "amountOfPlayers is not within: [2, 6]";
         Exception exception = assertThrows(IllegalArgumentException.class,
-                () -> unitUnderTest.initializePlayersList(emptyPlayerColorList, amountOfPlayers));
+                () -> unitUnderTest.initializePlayersList(emptyPlayerColorList, illegalInput));
 
         String actualMessage = exception.getMessage();
         assertEquals(expectedMessage, actualMessage);
