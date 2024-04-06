641fdf4fc9ad262a44e96eb5a6277600538ad3ef
wynessgp <wynessgp@rose-hulman.edu>
2024-04-05 20:00:41 -0500

    Noisy comment removal from DieRollParserTest

--- src/test/java/domain/DieRollParserTest.java
+++ src/test/java/domain/DieRollParserTest.java
@@ -27,50 +27,38 @@ public class DieRollParserTest {
     @ParameterizedTest
     @ValueSource(ints = {-1, 0, 4})
     public void test01_rollAttackerDice_tooFewOrTooManyDice_expectException(int illegalInput) {
-        // variable setup
         DieRollParser unitUnderTest = new DieRollParser();
 
-        // preliminary operation:
-        // assertTrue(unitUnderTest.buildDiceLists());
-
-        // perform the operation
         String expectedMessage = "Valid amount of dice is in the range [1, 3]";
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> unitUnderTest.rollAttackerDice(illegalInput));
 
-        // assert
         String actualMessage = exception.getMessage();
         assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
     public void test02_rollAttackerDice_rollThreeDice_expectSortedListOfSizeThree() {
-        // variable setup
         Random random = new Random();
 
         Die firstDie = EasyMock.mock(Die.class);
         Die secondDie = EasyMock.mock(Die.class);
         Die thirdDie = EasyMock.mock(Die.class);
 
-        // record - note these are in UNSORTED order!
+        // put in unsorted order on purpose, to ensure the result actually sorts them.
         EasyMock.expect(firstDie.rollSingleDie(random)).andReturn(1);
         EasyMock.expect(secondDie.rollSingleDie(random)).andReturn(3);
         EasyMock.expect(thirdDie.rollSingleDie(random)).andReturn(2);
 
-        // provide the underlying dice collection to our class.
         DieRollParser unitUnderTest = new DieRollParser(random,
                 List.of(firstDie, secondDie, thirdDie), List.of());
 
-        // replay
         EasyMock.replay(firstDie, secondDie, thirdDie);
 
-        // do regular JUnit test calculations
         List<Integer> actual = unitUnderTest.rollAttackerDice(3);
 
-        // verify
         EasyMock.verify(firstDie, secondDie, thirdDie);
 
-        // do regular JUnit assertions
         assertEquals(3, actual.size());
         assertTrue(isSortedInNonIncreasingOrder(actual));
         assertEquals(List.of(3, 2, 1), actual);
@@ -78,30 +66,23 @@ public class DieRollParserTest {
 
     @Test
     public void test03_rollAttackerDice_rollTwoDice_expectSortedListOfSizeTwo() {
-        // variable setup
         Random random = new Random();
 
         Die firstDie = EasyMock.mock(Die.class);
         Die secondDie = EasyMock.mock(Die.class);
 
-        // record
         EasyMock.expect(firstDie.rollSingleDie(random)).andReturn(4);
         EasyMock.expect(secondDie.rollSingleDie(random)).andReturn(4);
 
-        // provide the underlying dice collection to our class.
         DieRollParser unitUnderTest = new DieRollParser(random,
                 List.of(firstDie, secondDie), List.of());
 
-        // replay
         EasyMock.replay(firstDie, secondDie);
 
-        // do regular JUnit test calculations
         List<Integer> actual = unitUnderTest.rollAttackerDice(2);
 
-        // verify
         EasyMock.verify(firstDie, secondDie);
 
-        // do regular JUnit assertions
         assertEquals(2, actual.size());
         assertTrue(isSortedInNonIncreasingOrder(actual));
         assertEquals(List.of(4, 4), actual);
@@ -109,42 +90,29 @@ public class DieRollParserTest {
 
     @Test
     public void test04_rollAttackerDice_rollOneDie_expectListOfSizeOne() {
-        // variable setup
         Random random = new Random();
 
         Die firstDie = EasyMock.mock(Die.class);
-
-        // record
         EasyMock.expect(firstDie.rollSingleDie(random)).andReturn(3);
 
-        // provide the underlying dice collection to our class.
         DieRollParser unitUnderTest = new DieRollParser(random, List.of(firstDie), List.of());
 
-        // replay
         EasyMock.replay(firstDie);
 
-        // do regular JUnit test calculations
         List<Integer> actual = unitUnderTest.rollAttackerDice(1);
 
-        // verify
         EasyMock.verify(firstDie);
 
-        // do regular JUnit assertions
         assertEquals(1, actual.size());
         assertEquals(List.of(3), actual);
     }
 
     @Test
     public void test05_rollAttackerDice_rollTwoUnsetValueDice_expectSortedListOfSizeTwo() {
-        // variable setup
         DieRollParser unitUnderTest = new DieRollParser();
 
-        // ok, create the dice lists.
-        // assertTrue(unitUnderTest.buildDiceLists());
-
-        // now attempt to roll the dice to ensure the UUT did its job
         List<Integer> actual = unitUnderTest.rollAttackerDice(2);
-        // we don't care what the actual numbers are in the output; just that it's sorted.
+
         assertEquals(2, actual.size());
         assertTrue(isSortedInNonIncreasingOrder(actual));
     }
@@ -152,48 +120,35 @@ public class DieRollParserTest {
     @ParameterizedTest
     @ValueSource(ints = {-1, 0, 3})
     public void test06_rollDefenderDice_tooFewOrTooManyDice_expectException(int illegalInput) {
-        // variable setup
         DieRollParser unitUnderTest = new DieRollParser();
 
-        // preliminary operation:
-        // assertTrue(unitUnderTest.buildDiceLists());
-
-        // perform the operation
         String expectedMessage = "Valid amount of dice is in the range [1, 2]";
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> unitUnderTest.rollDefenderDice(illegalInput));
 
-        // assert
         String actualMessage = exception.getMessage();
         assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
     public void test07_rollDefenderDice_rollTwoDice_expectSortedListOfSizeTwo() {
-        // variable setup
         Random random = new Random();
 
         Die firstDie = EasyMock.mock(Die.class);
         Die secondDie = EasyMock.mock(Die.class);
 
-        // record - note these are not in sorted order!
         EasyMock.expect(firstDie.rollSingleDie(random)).andReturn(2);
         EasyMock.expect(secondDie.rollSingleDie(random)).andReturn(3);
 
-        // provide the underlying dice collection to our class.
         DieRollParser unitUnderTest = new DieRollParser(random,
                 List.of(), List.of(firstDie, secondDie));
 
-        // replay
         EasyMock.replay(firstDie, secondDie);
 
-        // do regular JUnit test calculations
         List<Integer> actual = unitUnderTest.rollDefenderDice(2);
 
-        // verify
         EasyMock.verify(firstDie, secondDie);
 
-        // do regular JUnit assertions
         assertEquals(2, actual.size());
         assertTrue(isSortedInNonIncreasingOrder(actual));
         assertEquals(List.of(3, 2), actual);
@@ -201,30 +156,23 @@ public class DieRollParserTest {
 
     @Test
     public void test08_rollDefenderDice_rollTwoDiceIdenticalValues_expectSortedListOfSizeTwo() {
-        // variable setup
         Random random = new Random();
 
         Die firstDie = EasyMock.mock(Die.class);
         Die secondDie = EasyMock.mock(Die.class);
 
-        // record - note these are not in sorted order!
         EasyMock.expect(firstDie.rollSingleDie(random)).andReturn(4);
         EasyMock.expect(secondDie.rollSingleDie(random)).andReturn(4);
 
-        // provide the underlying dice collection to our class.
         DieRollParser unitUnderTest = new DieRollParser(random,
                 List.of(), List.of(firstDie, secondDie));
 
-        // replay
         EasyMock.replay(firstDie, secondDie);
 
-        // do regular JUnit test calculations
         List<Integer> actual = unitUnderTest.rollDefenderDice(2);
 
-        // verify
         EasyMock.verify(firstDie, secondDie);
 
-        // do regular JUnit assertions
         assertEquals(2, actual.size());
         assertTrue(isSortedInNonIncreasingOrder(actual));
         assertEquals(List.of(4, 4), actual);
@@ -232,53 +180,39 @@ public class DieRollParserTest {
 
     @Test
     public void test09_rollDefenderDice_rollOneDie_expectListOfSizeOne() {
-        // variable setup
         Random random = new Random();
         Die firstDie = EasyMock.mock(Die.class);
 
-        // record
         EasyMock.expect(firstDie.rollSingleDie(random)).andReturn(3);
 
-        // provide the underlying dice collection to our class.
         DieRollParser unitUnderTest = new DieRollParser(random,
                 List.of(), List.of(firstDie));
 
-        // replay
         EasyMock.replay(firstDie);
 
-        // do regular JUnit test calculations
         List<Integer> actual = unitUnderTest.rollDefenderDice(1);
 
-        // verify
         EasyMock.verify(firstDie);
 
-        // do regular JUnit assertions
         assertEquals(1, actual.size());
         assertEquals(List.of(3), actual);
     }
 
     @Test
     public void test10_rollDefenderDice_rollTwoUnsetValueDice_expectSortedListOfSizeTwo() {
-        // variable setup
         DieRollParser unitUnderTest = new DieRollParser();
 
-        // ok, create the dice lists.
-        // assertTrue(unitUnderTest.buildDiceLists());
-
-        // now attempt to roll the dice to ensure the UUT did its job
         List<Integer> actual = unitUnderTest.rollDefenderDice(2);
-        // we don't care what the actual numbers are in the output; just that it's sorted.
+
         assertEquals(2, actual.size());
         assertTrue(isSortedInNonIncreasingOrder(actual));
     }
 
     @Test
     public void test11_validateSortIsInNonIncreasingOrder_listOfSizeOne_expectTrue() {
-        // variable setup
         DieRollParser unitUnderTest = new DieRollParser();
         List<Integer> list = List.of(1);
 
-        // assert statement
         assertTrue(unitUnderTest.validateSortIsInNonIncreasingOrder(list));
     }
 
@@ -286,11 +220,9 @@ public class DieRollParserTest {
     @CsvSource({"4, 2, true", "3, 3, true", "2, 4, false"})
     public void test12_validateSortIsInNonIncreasingOrder_listOfSizeTwo_resultVaries(
             int listElementOne, int listElementTwo, boolean expectedResult) {
-        // variable setup
         DieRollParser unitUnderTest = new DieRollParser();
         List<Integer> list = List.of(listElementOne, listElementTwo);
 
-        // assert statement
         assertEquals(expectedResult, unitUnderTest.validateSortIsInNonIncreasingOrder(list));
     }
 
@@ -299,22 +231,18 @@ public class DieRollParserTest {
     public void test13_validateSortIsInNonIncreasingOrder_listOfSizeThree_resultVaries(
             int listElementOne, int listElementTwo,
             int listElementThree, boolean expectedResult) {
-        // variable setup
         DieRollParser unitUnderTest = new DieRollParser();
         List<Integer> list = List.of(listElementOne, listElementTwo, listElementThree);
 
-        // assert statement
         assertEquals(expectedResult, unitUnderTest.validateSortIsInNonIncreasingOrder(list));
     }
 
     @Test
     public void test14_generateBattleResults_emptyAttackerRolls_expectException() {
-        // variable setup
         DieRollParser unitUnderTest = new DieRollParser();
         List<Integer> defenderRolls = List.of(5);
         List<Integer> attackerRolls = List.of();
 
-        // perform the operation
         String expectedMessage = "Both arguments must have at least one element";
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> unitUnderTest.generateBattleResults(defenderRolls, attackerRolls));
@@ -325,12 +253,10 @@ public class DieRollParserTest {
 
     @Test
     public void test15_generateBattleResults_emptyDefenderRolls_expectException() {
-        // variable setup
         DieRollParser unitUnderTest = new DieRollParser();
         List<Integer> defenderRolls = List.of();
         List<Integer> attackerRolls = List.of(5);
 
-        // perform the operation
         String expectedMessage = "Both arguments must have at least one element";
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> unitUnderTest.generateBattleResults(defenderRolls, attackerRolls));
@@ -341,12 +267,10 @@ public class DieRollParserTest {
 
     @Test
     public void test16_generateBattleResults_bothInputsEmpty_expectException() {
-        // variable setup
         DieRollParser unitUnderTest = new DieRollParser();
         List<Integer> defenderRolls = List.of();
         List<Integer> attackerRolls = List.of();
 
-        // perform the operation
         String expectedMessage = "Both arguments must have at least one element";
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> unitUnderTest.generateBattleResults(defenderRolls, attackerRolls));
@@ -361,7 +285,6 @@ public class DieRollParserTest {
         List<Integer> defenderRolls = List.of(1, 2);
         List<Integer> attackerRolls = List.of(5);
 
-        // perform the operation
         String expectedMessage = "defenderRolls are not sorted in non-increasing order";
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> unitUnderTest.generateBattleResults(defenderRolls, attackerRolls));
@@ -376,7 +299,6 @@ public class DieRollParserTest {
         List<Integer> defenderRolls = List.of(5);
         List<Integer> attackerRolls = List.of(1, 2);
 
-        // perform the operation
         String expectedMessage = "attackerRolls are not sorted in non-increasing order";
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> unitUnderTest.generateBattleResults(defenderRolls, attackerRolls));
@@ -391,11 +313,9 @@ public class DieRollParserTest {
         List<Integer> defenderRolls = List.of(5);
         List<Integer> attackerRolls = List.of(5);
 
-        // perform the operation
         List<BattleResult> actual = unitUnderTest.generateBattleResults(
                 defenderRolls, attackerRolls);
 
-        // assertions
         assertEquals(1, actual.size());
         assertEquals(List.of(BattleResult.DEFENDER_VICTORY), actual);
     }
@@ -406,11 +326,9 @@ public class DieRollParserTest {
         List<Integer> defenderRolls = List.of(6, 6);
         List<Integer> attackerRolls = List.of(1);
 
-        // perform the operation
         List<BattleResult> actual = unitUnderTest.generateBattleResults(
                 defenderRolls, attackerRolls);
 
-        // assertions
         assertEquals(1, actual.size());
         assertEquals(List.of(BattleResult.DEFENDER_VICTORY), actual);
     }
@@ -421,11 +339,9 @@ public class DieRollParserTest {
         List<Integer> defenderRolls = List.of(6, 6);
         List<Integer> attackerRolls = List.of(5, 3, 2);
 
-        // perform the operation
         List<BattleResult> actual = unitUnderTest.generateBattleResults(
                 defenderRolls, attackerRolls);
 
-        // assertions
         assertEquals(2, actual.size());
         assertEquals(List.of(BattleResult.DEFENDER_VICTORY, BattleResult.DEFENDER_VICTORY),
                 actual);
@@ -437,11 +353,9 @@ public class DieRollParserTest {
         List<Integer> defenderRolls = List.of(6, 2);
         List<Integer> attackerRolls = List.of(4, 4, 3);
 
-        // perform the operation
         List<BattleResult> actual = unitUnderTest.generateBattleResults(
                 defenderRolls, attackerRolls);
 
-        // assertions
         assertEquals(2, actual.size());
         assertEquals(List.of(BattleResult.DEFENDER_VICTORY, BattleResult.ATTACKER_VICTORY),
                 actual);
@@ -453,11 +367,9 @@ public class DieRollParserTest {
         List<Integer> defenderRolls = List.of(3);
         List<Integer> attackerRolls = List.of(4);
 
-        // perform the operation
         List<BattleResult> actual = unitUnderTest.generateBattleResults(
                 defenderRolls, attackerRolls);
 
-        // assertions
         assertEquals(1, actual.size());
         assertEquals(List.of(BattleResult.ATTACKER_VICTORY), actual);
     }
@@ -468,11 +380,9 @@ public class DieRollParserTest {
         List<Integer> defenderRolls = List.of(3, 2);
         List<Integer> attackerRolls = List.of(5, 4);
 
-        // perform the operation
         List<BattleResult> actual = unitUnderTest.generateBattleResults(
                 defenderRolls, attackerRolls);
 
-        // assertions
         assertEquals(2, actual.size());
         assertEquals(List.of(BattleResult.ATTACKER_VICTORY, BattleResult.ATTACKER_VICTORY),
                 actual);
@@ -484,7 +394,6 @@ public class DieRollParserTest {
             int illegalInput) {
         DieRollParser unitUnderTest = new DieRollParser();
 
-        // perform the operation
         String expectedMessage = "Valid amount of dice is in the range [2, 6]";
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> unitUnderTest.rollDiceToDeterminePlayerOrder(illegalInput));
@@ -497,14 +406,12 @@ public class DieRollParserTest {
     @ValueSource(ints = {2, 3, 4, 5, 6})
     public void test26_rollDiceToDeterminePlayerOrder_unstructuredOutput_expectNoDuplicates(
             int numDiceToRoll) {
-        // variable setup
         DieRollParser unitUnderTest = new DieRollParser();
 
         // the main point of this one is to see, if given no "set" dice values, we can
         // get the method to return a list with no duplicates.
         List<Integer> actual = unitUnderTest.rollDiceToDeterminePlayerOrder(numDiceToRoll);
 
-        // assertions
         assertEquals(numDiceToRoll, actual.size());
         assertEquals(numDiceToRoll, Set.copyOf(actual).size());
     }
