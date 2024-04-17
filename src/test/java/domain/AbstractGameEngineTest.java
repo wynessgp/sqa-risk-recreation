15d0f21f4f433137d130c3b61a3e77b03b8cc601
wynessgp <wynessgp@rose-hulman.edu>
2024-04-16 22:56:42 -0400

    Refactored test 22 using private helpers for mocked objects

--- src/test/java/domain/AbstractGameEngineTest.java
+++ src/test/java/domain/AbstractGameEngineTest.java
@@ -452,13 +452,8 @@ public class AbstractGameEngineTest {
     @EnumSource(TerritoryType.class)
     public void test22_checkIfPlayerOwnsTerritory_nonSetupPlayerOwnsTerritory_inputIsBluePlayer_expectFalse(
             TerritoryType relevantTerritory) {
-        TerritoryGraph mockedGraph = EasyMock.createMock(TerritoryGraph.class);
-        Territory mockedTerritory = EasyMock.createMock(Territory.class);
-
-        EasyMock.expect(mockedGraph.getTerritory(relevantTerritory)).andReturn(mockedTerritory);
-        EasyMock.expect(mockedTerritory.getPlayerInControl()).andReturn(PlayerColor.RED);
-
-        EasyMock.replay(mockedGraph, mockedTerritory);
+        Territory mockedTerritory = createMockedTerritoryWithExpectations(PlayerColor.RED);
+        TerritoryGraph mockedGraph = createMockedGraphWithExpectations(relevantTerritory, mockedTerritory, 1);
 
         GameEngine unitUnderTest = new WorldDominationGameEngine();
         unitUnderTest.provideMockedTerritoryGraph(mockedGraph);
