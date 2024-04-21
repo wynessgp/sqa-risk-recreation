af51929bed2e904b4e4cff47bd26092c9875f889
Canon <22649817+cm090@users.noreply.github.com>
2024-04-21 14:00:46 -0400

    test04_ownsTerritory_withOneMatchAndOneMismatch_returnsTrue passes

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -44,10 +45,40 @@ public class PlayerTest {
 
     @ParameterizedTest
     @MethodSource("territoryTypeGenerator")
-    public void test02_ownsTerritory_ownsAll_returnsTrue(TerritoryType territory) {
+    public void test03_ownsTerritory_ownsAll_returnsTrue(TerritoryType territory) {
         Player player = new Player();
         player.setTerritories(Set.of(TerritoryType.values()));
         assertTrue(player.ownsTerritory(territory));
+    }
+
+    private static Stream<Arguments> territoryTypeCombinationsGenerator() {
+        Set<Set<TerritoryType>> territoriesNoDuplicates = new HashSet<>();
+        for (TerritoryType startingTerritory : TerritoryType.values()) {
+            for (TerritoryType endingTerritory : TerritoryType.values()) {
+                if (endingTerritory != startingTerritory) {
+                    Set<TerritoryType> territoryPair = new HashSet<>();
+                    territoryPair.add(startingTerritory);
+                    territoryPair.add(endingTerritory);
+                    territoriesNoDuplicates.add(territoryPair);
+                }
+            }
+        }
+        Set<Arguments> territoryArguments = new HashSet<>();
+        for (Set<TerritoryType> territoryPair : territoriesNoDuplicates) {
+            Iterator<TerritoryType> iterator = territoryPair.iterator();
+            TerritoryType startingTerritoryType = iterator.next();
+            TerritoryType endingTerritoryType = iterator.next();
+            territoryArguments.add(Arguments.of(startingTerritoryType, endingTerritoryType));
+        }
+        return territoryArguments.stream();
+    }
+
+    @ParameterizedTest
+    @MethodSource("territoryTypeCombinationsGenerator")
+    public void test04_ownsTerritory_withOneMatchAndOneMismatch_returnsTrue(TerritoryType first, TerritoryType second) {
+        Player player = new Player();
+        player.setTerritories(Set.of(first, second));
+        assertTrue(player.ownsTerritory(first));
     }
 
 }
