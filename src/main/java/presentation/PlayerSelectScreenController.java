68a1b112353f28e82922619c5fdf718cefa25fdc
Canon <22649817+cm090@users.noreply.github.com>
2024-04-21 12:24:13 -0400

    Get PlayerColors from button presses

--- src/main/java/presentation/PlayerSelectScreenController.java
+++ src/main/java/presentation/PlayerSelectScreenController.java
@@ -1,37 +1,22 @@
 package presentation;
 
+import domain.PlayerColor;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class PlayerSelectScreenController {
-    @FXML
-    private Button blackPlayer;
-    @FXML
-    private Button redPlayer;
-    @FXML
-    private Button yellowPlayer;
-    @FXML
-    private Button bluePlayer;
-    @FXML
-    private Button greenPlayer;
-    @FXML
-    private Button purplePlayer;
-
-    public void initialize() {
-        blackPlayer.setOnAction(e -> onPlayerSelect(blackPlayer));
-        redPlayer.setOnAction(e -> onPlayerSelect(redPlayer));
-        yellowPlayer.setOnAction(e -> onPlayerSelect(yellowPlayer));
-        bluePlayer.setOnAction(e -> onPlayerSelect(bluePlayer));
-        greenPlayer.setOnAction(e -> onPlayerSelect(greenPlayer));
-        purplePlayer.setOnAction(e -> onPlayerSelect(purplePlayer));
-    }
 
     @FXML
     private void onBackButtonClick() {
         SceneController.getInstance().activate(SceneType.START);
     }
 
-    private void onPlayerSelect(Button button) {
+    @FXML
+    private void onPlayerSelect(ActionEvent e) {
+        String buttonText = ((Button) e.getSource()).getText();
+        PlayerColor player = PlayerColor.valueOf(buttonText.toUpperCase());
+        System.out.println("Player selected: " + player);
     }
 
 }
