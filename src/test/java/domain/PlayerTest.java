d14c6d8eefb5e61d0b5aff54f157ea543035ab80
Canon <22649817+cm090@users.noreply.github.com>
2024-04-21 13:50:44 -0400

    Refactor test00_ownsTerritory_withNoneOwned_returnsFalse to run with all territory types

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -2,14 +2,22 @@ package domain;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class PlayerTest {
 
-    @Test
-    public void test00_ownsTerritory_withNoneOwned_returnsFalse() {
+    private static Stream<Arguments> territoryTypeGenerator() {
+        return Stream.of(Arrays.stream(TerritoryType.values()).map(Arguments::of).toArray(Arguments[]::new));
+    }
+
+    @ParameterizedTest
+    @MethodSource("territoryTypeGenerator")
+    public void test00_ownsTerritory_withNoneOwned_returnsFalse(TerritoryType territory) {
         Player player = new Player();
-        TerritoryType territory = TerritoryType.ALASKA;
         assertFalse(player.ownsTerritory(territory));
     }
 
