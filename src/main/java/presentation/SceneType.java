38c5084431d6442b9f1bc2fec29cde6a6fd4e9cd
Canon <22649817+cm090@users.noreply.github.com>
2024-04-22 08:30:04 -0400

    Create player shuffle screen

--- src/main/java/presentation/SceneType.java
+++ src/main/java/presentation/SceneType.java
@@ -3,6 +3,7 @@ package presentation;
 public enum SceneType {
     START("start_screen"),
     PLAYER_SELECT("player_select_screen"),
+    SHUFFLE("player_shuffle_screen"),
     GAME("game_map_screen");
 
     private final String sceneName;
