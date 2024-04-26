95e25477c9fb1cc2945f5f6649fc62229c6eae44
Canon <22649817+cm090@users.noreply.github.com>
2024-04-26 08:57:33 -0400

    Fix CheckStyle warning

--- src/test/java/domain/PlayerColorTest.java
+++ src/test/java/domain/PlayerColorTest.java
@@ -2,10 +2,10 @@ package domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import java.util.stream.Stream;
 
 public class PlayerColorTest {
 
