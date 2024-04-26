d7859e7817e87fa5d04641bea2d10a1513a527e4
wynessgp <wynessgp@rose-hulman.edu>
2024-04-25 23:49:41 -0400

    test02PlaceNewArmiesInTerritoryScramblePhaseDoubleClaimingTerritoryExpectException passes

--- src/test/java/domain/WorldDominationGameEngineIntegrationTest.java
+++ src/test/java/domain/WorldDominationGameEngineIntegrationTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
+import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -54,5 +56,41 @@ public class WorldDominationGameEngineIntegrationTest {
         for (TerritoryType territory : TerritoryType.values()) {
             assertTrue(unitUnderTest.checkIfPlayerOwnsTerritory(territory, PlayerColor.SETUP));
         }
+    }
+
+    private DieRollParser generateMockedParser(List<PlayerColor> players) {
+        DieRollParser parser = EasyMock.createMock(DieRollParser.class);
+        List<Integer> dieRolls = new ArrayList<>();
+        for (int i = players.size(); i > 0; i--) {
+            dieRolls.add(i);
+        }
+        EasyMock.expect(parser.rollDiceToDeterminePlayerOrder(players.size())).andReturn(dieRolls);
+        EasyMock.replay(parser);
+        return parser;
+    }
+
+    @Test
+    public void test02_placeNewArmiesInTerritoryScramblePhase_doubleClaimingTerritory_expectException() {
+        List<PlayerColor> playerColors = List.of(PlayerColor.RED, PlayerColor.PURPLE, PlayerColor.BLUE);
+        // we want to ensure our player colors list ordering stays the same after shuffling, so we need a mock.
+        DieRollParser parser = generateMockedParser(playerColors);
+        WorldDominationGameEngine unitUnderTest = new WorldDominationGameEngine(playerColors, parser);
+        TerritoryType targetTerritory = TerritoryType.ALASKA;
+        int numArmiesToPlace = 1;
+
+        // since it is an expectation that the WorldDominationGameEngine handles whose turn it is FOR us,
+        // we will try placing an army as RED, then the current player should be PURPLE.
+        assertTrue(unitUnderTest.placeNewArmiesInTerritory(targetTerritory, numArmiesToPlace));
+
+        assertEquals(PlayerColor.PURPLE, unitUnderTest.getCurrentPlayer());
+
+        // now we'll try to place an army in the same place RED just did.
+        String expectedMessage = "Cannot place armies in a claimed territory until the scramble phase is over";
+        Exception exception = assertThrows(IllegalStateException.class,
+                () -> unitUnderTest.placeNewArmiesInTerritory(targetTerritory, numArmiesToPlace));
+
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+        EasyMock.verify(parser);
     }
 }
