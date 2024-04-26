958d90c13c7175300cfe14be3367a3d634032f4e
Canon <22649817+cm090@users.noreply.github.com>
2024-04-26 09:38:15 -0400

    Refactor test00_toString_withValidResult_returnsProperString to test all PieceTypes

--- src/main/java/domain/PieceType.java
+++ src/main/java/domain/PieceType.java
@@ -6,7 +6,8 @@ public enum PieceType {
     ARTILLERY;
 
     public String toString() {
-        return "Infantry";
+        String name = this.name();
+        return name.charAt(0) + name.substring(1).toLowerCase();
     }
 
 }
--- src/test/java/domain/PieceTypeTest.java
+++ src/test/java/domain/PieceTypeTest.java
@@ -2,14 +2,24 @@ package domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class PieceTypeTest {
 
-    @Test
-    public void test00_toString_withValidResult_returnsProperString() {
-        PieceType piece = PieceType.INFANTRY;
-        String expected = "Infantry";
+    private static Stream<Arguments> pieceTypeGenerator() {
+        return Stream.of(
+                Arguments.of(PieceType.INFANTRY, "Infantry"),
+                Arguments.of(PieceType.CAVALRY, "Cavalry"),
+                Arguments.of(PieceType.ARTILLERY, "Artillery")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("pieceTypeGenerator")
+    public void test00_toString_withValidResult_returnsProperString(PieceType piece, String expected) {
         String result = piece.toString();
         assertEquals(expected, result);
     }
