f7ba8c67939aad01a9539e874993550ebe8200db
Canon <22649817+cm090@users.noreply.github.com>
2024-04-28 13:21:27 -0400

    Allow players to claim territories

--- src/main/java/presentation/GameMapScreenController.java
+++ src/main/java/presentation/GameMapScreenController.java
@@ -1,6 +1,7 @@
 package presentation;
 
 import domain.TerritoryType;
+import domain.WorldDominationGameEngine;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -10,21 +11,33 @@ import javafx.scene.control.DialogPane;
 public class GameMapScreenController {
     @FXML
     private DialogPane claimTerritoryDialog;
+    private WorldDominationGameEngine gameEngine;
+    private TerritoryType selectedTerritory;
+    private Button selectedButton;
 
     @FXML
     private void initialize() {
         claimTerritoryDialog.setVisible(false);
+        this.gameEngine = SceneController.getInstance().getGameEngine();
         claimTerritoryDialog.lookupButton(ButtonType.YES).addEventHandler(ActionEvent.ACTION, event ->
-                claimTerritoryDialog.setVisible(false));
+                handleClaimTerritory());
         claimTerritoryDialog.lookupButton(ButtonType.NO).addEventHandler(ActionEvent.ACTION, event ->
                 claimTerritoryDialog.setVisible(false));
     }
 
+    private void handleClaimTerritory() {
+        selectedButton.styleProperty().setValue("-fx-background-color: " + gameEngine.getCurrentPlayer());
+        selectedButton.setText("1");
+        selectedButton.setDisable(true);
+        gameEngine.placeNewArmiesInTerritory(selectedTerritory, 1);
+        claimTerritoryDialog.setVisible(false);
+    }
+
     @FXML
     private void handleTerritoryButtonClick(ActionEvent event) {
-        Button button = (Button) event.getSource();
-        TerritoryType territory = TerritoryType.valueOf(button.getAccessibleText());
-        claimTerritoryDialog.setContentText("Would you like to claim " + territory + "?");
+        selectedButton = (Button) event.getSource();
+        selectedTerritory = TerritoryType.valueOf(selectedButton.getAccessibleText());
+        claimTerritoryDialog.setContentText("Would you like to claim " + selectedTerritory + "?");
         claimTerritoryDialog.setVisible(true);
     }
 
