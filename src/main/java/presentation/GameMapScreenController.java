630bf523b64cb22ce523972705958a85ec12680b
Canon <22649817+cm090@users.noreply.github.com>
2024-04-28 13:35:06 -0400

    Display game status

--- src/main/java/presentation/GameMapScreenController.java
+++ src/main/java/presentation/GameMapScreenController.java
@@ -7,38 +7,50 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
 
 public class GameMapScreenController {
     @FXML
     private DialogPane claimTerritoryDialog;
+    @FXML
+    private Label currentPlayerColor;
+    @FXML
+    private Label currentPhase;
     private WorldDominationGameEngine gameEngine;
     private TerritoryType selectedTerritory;
     private Button selectedButton;
 
     @FXML
     private void initialize() {
-        claimTerritoryDialog.setVisible(false);
+        this.claimTerritoryDialog.setVisible(false);
         this.gameEngine = SceneController.getInstance().getGameEngine();
+        updatePlayerAndPhase();
         claimTerritoryDialog.lookupButton(ButtonType.YES).addEventHandler(ActionEvent.ACTION, event ->
                 handleClaimTerritory());
         claimTerritoryDialog.lookupButton(ButtonType.NO).addEventHandler(ActionEvent.ACTION, event ->
                 claimTerritoryDialog.setVisible(false));
     }
 
+    private void updatePlayerAndPhase() {
+        this.currentPlayerColor.setText(this.gameEngine.getCurrentPlayer().toString());
+        this.currentPhase.setText(this.gameEngine.getCurrentGamePhase().toString());
+    }
+
     private void handleClaimTerritory() {
-        selectedButton.styleProperty().setValue("-fx-background-color: " + gameEngine.getCurrentPlayer());
-        selectedButton.setText("1");
-        selectedButton.setDisable(true);
-        gameEngine.placeNewArmiesInTerritory(selectedTerritory, 1);
-        claimTerritoryDialog.setVisible(false);
+        this.selectedButton.styleProperty().setValue("-fx-background-color: " + gameEngine.getCurrentPlayer());
+        this.selectedButton.setText("");
+        this.selectedButton.setDisable(true);
+        this.gameEngine.placeNewArmiesInTerritory(this.selectedTerritory, 1);
+        this.claimTerritoryDialog.setVisible(false);
+        updatePlayerAndPhase();
     }
 
     @FXML
     private void handleTerritoryButtonClick(ActionEvent event) {
-        selectedButton = (Button) event.getSource();
-        selectedTerritory = TerritoryType.valueOf(selectedButton.getAccessibleText());
-        claimTerritoryDialog.setContentText("Would you like to claim " + selectedTerritory + "?");
-        claimTerritoryDialog.setVisible(true);
+        this.selectedButton = (Button) event.getSource();
+        this.selectedTerritory = TerritoryType.valueOf(this.selectedButton.getAccessibleText());
+        this.claimTerritoryDialog.setContentText("Would you like to claim " + this.selectedTerritory + "?");
+        this.claimTerritoryDialog.setVisible(true);
     }
 
 }
