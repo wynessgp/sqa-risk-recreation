4d8f0f71d0b98c887dab12a9af665d857ce0301f
Canon <22649817+cm090@users.noreply.github.com>
2024-04-28 13:11:51 -0400

    Clicking territory button shows claim dialog

--- src/main/java/presentation/GameMapScreenController.java
+++ src/main/java/presentation/GameMapScreenController.java
@@ -1,100 +1,31 @@
 package presentation;
 
+import domain.TerritoryType;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DialogPane;
 
 public class GameMapScreenController {
-    @FXML
-    private Button alaskaButton;
-    @FXML
-    private Button albertaButton;
-    @FXML
-    private Button centralAmericaButton;
-    @FXML
-    private Button easternUnitedStatesButton;
-    @FXML
-    private Button greenlandButton;
-    @FXML
-    private Button northwestTerritoryButton;
-    @FXML
-    private Button ontarioButton;
-    @FXML
-    private Button quebecButton;
-    @FXML
-    private Button westernUnitedStatesButton;
-    @FXML
-    private Button argentinaButton;
-    @FXML
-    private Button brazilButton;
-    @FXML
-    private Button peruButton;
-    @FXML
-    private Button venezuelaButton;
-    @FXML
-    private Button ukraineButton;
-    @FXML
-    private Button greatBritainButton;
-    @FXML
-    private Button icelandButton;
-    @FXML
-    private Button northernEuropeButton;
-    @FXML
-    private Button scandinaviaButton;
-    @FXML
-    private Button southernEuropeButton;
-    @FXML
-    private Button westernEuropeButton;
-    @FXML
-    private Button congoButton;
-    @FXML
-    private Button eastAfricaButton;
-    @FXML
-    private Button egyptButton;
-    @FXML
-    private Button madagascarButton;
-    @FXML
-    private Button northAfricaButton;
-    @FXML
-    private Button southAfricaButton;
-    @FXML
-    private Button afghanistanButton;
-    @FXML
-    private Button chinaButton;
-    @FXML
-    private Button indiaButton;
-    @FXML
-    private Button irkutskButton;
-    @FXML
-    private Button japanButton;
-    @FXML
-    private Button kamchatkaButton;
-    @FXML
-    private Button middleEastButton;
-    @FXML
-    private Button mongoliaButton;
-    @FXML
-    private Button siamButton;
-    @FXML
-    private Button siberiaButton;
-    @FXML
-    private Button uralButton;
-    @FXML
-    private Button yakutskButton;
-    @FXML
-    private Button easternAustraliaButton;
-    @FXML
-    private Button indonesiaButton;
-    @FXML
-    private Button newGuineaButton;
-    @FXML
-    private Button westernAustraliaButton;
     @FXML
     private DialogPane claimTerritoryDialog;
 
     @FXML
     private void initialize() {
         claimTerritoryDialog.setVisible(false);
+        claimTerritoryDialog.lookupButton(ButtonType.YES).addEventHandler(ActionEvent.ACTION, event ->
+                claimTerritoryDialog.setVisible(false));
+        claimTerritoryDialog.lookupButton(ButtonType.NO).addEventHandler(ActionEvent.ACTION, event ->
+                claimTerritoryDialog.setVisible(false));
+    }
+
+    @FXML
+    private void handleTerritoryButtonClick(ActionEvent event) {
+        Button button = (Button) event.getSource();
+        TerritoryType territory = TerritoryType.valueOf(button.getAccessibleText());
+        claimTerritoryDialog.setContentText("Would you like to claim " + territory + "?");
+        claimTerritoryDialog.setVisible(true);
     }
 
 }
