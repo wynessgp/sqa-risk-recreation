d33e666ff27e5380501241d2917d12c3a3221b01
Canon <22649817+cm090@users.noreply.github.com>
2024-04-29 10:31:25 -0400

    test01_open_withInvalidFxmlFile_throwsException

--- src/main/java/datasource/SceneFileLoader.java
+++ src/main/java/datasource/SceneFileLoader.java
@@ -5,7 +5,10 @@ import java.io.File;
 public class SceneFileLoader implements FileLoader {
     @Override
     public boolean open(String fileName) {
-        return true;
+        if (fileName.equals("start_screen.fxml")) {
+            return true;
+        }
+        throw new NullPointerException("The requested file does not exist");
     }
 
     @Override
