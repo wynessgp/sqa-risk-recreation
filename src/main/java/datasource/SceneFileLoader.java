c0acc5102ff94dae60de99930b0d42cb0ff5b8e7
Canon <22649817+cm090@users.noreply.github.com>
2024-04-29 10:56:18 -0400

    Fix CheckStyle warnings

--- src/main/java/datasource/SceneFileLoader.java
+++ src/main/java/datasource/SceneFileLoader.java
@@ -10,18 +10,21 @@ public class SceneFileLoader implements FileLoader {
 
     @Override
     public boolean open(String fileName) {
-        Path path;
-        try {
-            URL filePath = getClass().getResource(fileName);
-            path = Paths.get(filePath.toURI());
-        } catch (Exception e) {
-            throw new NullPointerException("The requested file does not exist");
-        }
+        Path path = generatePathObject(fileName);
         if (!path.toString().endsWith(".fxml")) {
             throw new IllegalArgumentException("The requested file is not an FXML file");
         }
         this.sceneFile = path.toFile();
         return true;
+    }
+
+    private Path generatePathObject(String fileName) {
+        try {
+            URL filePath = getClass().getResource(fileName);
+            return Paths.get(filePath.toURI());
+        } catch (Exception e) {
+            throw new NullPointerException("The requested file does not exist");
+        }
     }
 
     @Override
