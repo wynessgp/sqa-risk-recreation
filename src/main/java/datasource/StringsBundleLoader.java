62ef6eb6696b5e272edc76f748de15e4dfd7b969
Canon <22649817+cm090@users.noreply.github.com>
2024-05-02 15:41:32 -0400

    test02_open_withInvalidLocale_throwsException passes

--- src/main/java/datasource/StringsBundleLoader.java
+++ src/main/java/datasource/StringsBundleLoader.java
@@ -4,17 +4,21 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class StringsBundleLoader implements BundleLoader {
-    String locale;
+    ResourceBundle bundle;
 
     @Override
     public boolean open(String locale) {
-        this.locale = locale;
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("strings", new Locale(locale));
+        if (!resourceBundle.getLocale().toString().contains(locale.toLowerCase())) {
+            throw new IllegalArgumentException("The requested bundle does not exist");
+        }
+        this.bundle = resourceBundle;
         return true;
     }
 
     @Override
     public ResourceBundle getBundle() {
-        return ResourceBundle.getBundle("strings", new Locale(locale));
+        return this.bundle;
     }
 
 }
--- src/test/java/datasource/StringsBundleLoaderTest.java
+++ src/test/java/datasource/StringsBundleLoaderTest.java
@@ -2,10 +2,12 @@ package datasource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
 import java.util.ResourceBundle;
+import org.junit.jupiter.api.Test;
 
 public class StringsBundleLoaderTest {
 
@@ -35,6 +37,17 @@ public class StringsBundleLoaderTest {
         String expected = "Risk VI: Not the Architecture";
         String actual = bundle.getString("startScreen.title");
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test02_open_withInvalidLocale_throwsException() {
+        BundleLoader bundleLoader = new StringsBundleLoader();
+        String fileName = "Nonexistent";
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> bundleLoader.open(fileName));
+
+        String expectedMessage = "The requested bundle does not exist";
+        assertEquals(expectedMessage, exception.getMessage());
+        assertNull(bundleLoader.getBundle());
     }
 
 }
