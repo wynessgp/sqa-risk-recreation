91e905269d2053ea87a5c1815da0dd3d117389f3
Canon <22649817+cm090@users.noreply.github.com>
2024-05-03 11:26:24 -0400

    Refactor StringsBundleLoader to improve static access

--- src/main/java/datasource/StringsBundleLoader.java
+++ src/main/java/datasource/StringsBundleLoader.java
@@ -5,20 +5,32 @@ import java.util.ResourceBundle;
 
 public class StringsBundleLoader {
     private static final String BUNDLE_NAME = "strings";
-    private static String localeName = "";
+    private static final StringsBundleLoader INSTANCE = new StringsBundleLoader();
+    private String localeName = "";
 
-    public static boolean open(String locale) {
+    private boolean openBundle(String locale) {
         ResourceBundle resourceBundle = ResourceBundle.getBundle(BUNDLE_NAME, new Locale(locale));
         if (!resourceBundle.getLocale().toString().contains(locale.toLowerCase())) {
             localeName = "";
-            throw new IllegalArgumentException("The requested bundle does not exist");
+            return false;
         }
         localeName = locale;
         return true;
     }
 
+    private ResourceBundle getResourceBundle() {
+        return localeName.isEmpty() ? null : ResourceBundle.getBundle(BUNDLE_NAME, new Locale(localeName));
+    }
+
+    public static boolean open(String locale) {
+        if (!INSTANCE.openBundle(locale)) {
+            throw new IllegalArgumentException("The requested bundle does not exist");
+        }
+        return true;
+    }
+
     public static ResourceBundle getBundle() {
-        return localeName.isEmpty() ? null :  ResourceBundle.getBundle(BUNDLE_NAME, new Locale(localeName));
+        return INSTANCE.getResourceBundle();
     }
 
 }
