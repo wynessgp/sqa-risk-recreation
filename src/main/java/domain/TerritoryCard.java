f4cd30a299195b03b61b403031b01b2158d7cd8a
Canon <22649817+cm090@users.noreply.github.com>
2024-05-12 12:43:58 -0400

    Reduce visibility of domain classes

--- src/main/java/domain/TerritoryCard.java
+++ src/main/java/domain/TerritoryCard.java
@@ -1,23 +1,27 @@
 package domain;
 
-public class TerritoryCard implements Card {
+class TerritoryCard implements Card {
     private final TerritoryType territory;
     private final PieceType piece;
 
-    public TerritoryCard(TerritoryType territory, PieceType piece) {
+    TerritoryCard(TerritoryType territory, PieceType piece) {
         this.territory = territory;
         this.piece = piece;
     }
 
+    @Override
     public boolean matchesTerritory(TerritoryType territory) {
         return this.territory == territory;
     }
 
+    @Override
     public boolean matchesPieceType(PieceType piece) {
         return this.piece == piece;
     }
 
+    @Override
     public boolean isWild() {
         return false;
     }
+
 }
